9929ab50ad4fdb62972ecbcf30e21060d3a5e555
tennga <tenngawang6080@gmail.com>
2020-05-04 16:29:44 -0400

    added styled-compoent

---
+++ src/containers/Main.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Styled from 'styled-components';
+
+const MainContainer = Styled.div`
+
+`;
+
+const Main = () => {
+    <MainContainer>
+        
+    </MainContainer>
+    
+}
+
+export default Main;
